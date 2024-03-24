77841659c608c3a1f1e9b229bd75be94b7c5f80a
이효원 <129304827+ehyowon@users.noreply.github.com>
2024-03-24 22:24:40 +0900

    x변수를 y변수로 수정

--- dot_product.py
+++ dot_product.py
@@ -5,6 +5,6 @@ def dot_product(a,b):
 	return sum
 
 arr1 = [int(x) for x in input().split()]
-arr2 = [int(x) for x in input().split()]
+arr2 = [int(y) for y in input().split()]
 dot = dot_product(arr1,arr2)
 print(dot)
